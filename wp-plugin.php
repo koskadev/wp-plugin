b97652c52bf0650a48dd997d9fc29cff22ebcc54
Aloasfire <Aloasfire@icloud.com>
2015-11-27 18:45:59 -0500

    Update wp-plugin.php

--- wp-plugin.php
+++ wp-plugin.php
@@ -1,8 +1,12 @@
 <?php
 /**
-*Plugin Name : Koska Plugin
-*Plugin URI : https://koska.at/plugin
-*Author : Ben Koska
+*Plugin Name: Koska Plugin
+*Plugin URI: https://koska.at/plugin
+*Author: Ben Koska
+ *Author URI: ben.koska.at
+ * Version: Beta 0.0.1
+ * Licence: GPLv2
+ * Descroption: A Plugin for Koska.at
 */
 
 ?>
